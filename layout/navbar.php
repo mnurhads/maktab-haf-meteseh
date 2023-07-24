4ce196082acf154e71197659fd7ba4aeda37f092
mnurhad <nurhadi@nominavipindonesia.com>
2023-07-24 15:55:37 +0700

    view: update index home

---
+++ layout/navbar.php
@@ -0,0 +1,30 @@
+<div class="header-top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="logo-area">
+                    <a class="head-logo" href="<?= $log->baseUrl(); ?>"><img src="<?= $log->baseUrl(); ?>/assets/img/khidmah.png" alt="logo-alkhidmah" width="48px"/> <b>Maktab Haul Akbar Jateng - DIY 2023</b></a>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                <div class="header-top-menu">
+                    <ul class="nav navbar-nav notika-top-nav">
+                        <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span><i class="notika-icon notika-chat"></i></span></a>
+                            <div role="menu" class="dropdown-menu message-dd chat-dd animated zoomIn">
+                                <div class="hd-mg-tt">
+                                    <h2>FAQ</h2>
+                                </div>
+                                <div class="hd-mg-va">
+                                    <a href="#">Kontak Koordinator</a>
+                                </div>
+                                <div class="hd-mg-va">
+                                    <a href="#">Peta Maktab</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
